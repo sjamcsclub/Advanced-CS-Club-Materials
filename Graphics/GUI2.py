c34f3a797f0faebfa983599a35d55daa298828d8
coolcom200 <leonfattakhov@yahoo.com>
2016-10-10 17:34:11 -0400

    searchInternet

--- Graphics/GUI2.py
+++ Graphics/GUI2.py
@@ -1,43 +1,27 @@
 from tkinter import *
-
+import webbrowser
+MathTopics = ["Trigonometry", "Algebra", "Calculus", "Linear Algebra"]
 
 
 def send():
-    a = mode.get()
-    string = userIn.get()
-    if a == "reversed":
-        string = string[::-1]
 
-    userIn.delete(0, END)
-    screen.create_text(10, 10, text=string, anchor=NW)
-    screen.update()
+    selected = listb.curselection()
+    webbrowser.open("https://www.google.ca/#q="+MathTopics[selected[0]])  # google search query
 
 
-def cleardispOut():
-    dispOut.config(state="normal")
-    dispOut.delete("1.0", END)
-    dispOut.config(state="disabled")
+def insert():
+    listb.insert(END, *MathTopics)
 
 
 root = Tk()
 controlPanel = Frame(root)
-controlPanel.grid(row=1, column=1, sticky=NW)
+controlPanel.pack()
 
-userIn = Entry(controlPanel)
-userIn.grid(row=1, column=1)
+listb = Listbox(controlPanel, selectmode=SINGLE)
+listb.pack()
+insert()
 
 sendB = Button(controlPanel, text="send", command=send)
-sendB.grid(row=1, column=2)
-
-mode = StringVar()
-mode.set("reversed")
-outputMode = Checkbutton(controlPanel, text="Reverse", variable=mode, onvalue="reversed", offvalue=" ")
-outputMode.grid(row=1, column=3)
-
-output = Frame(root)
-output.grid(row=2, column=1, sticky=N)
-screen = Canvas(output)
-screen.grid(row=1, column=1, sticky=NW)
-
+sendB.pack()
 
 root.mainloop()
