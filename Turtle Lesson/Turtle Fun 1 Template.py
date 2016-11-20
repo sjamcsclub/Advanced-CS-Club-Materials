f68282af643daad6327d27dc396e5aea412769d0
coolcom200 <leonfattakhov@yahoo.com>
2016-11-20 18:23:54 -0500

    Turtle Graphics

--- Turtle Lesson/Turtle Fun 1 Template.py
+++ Turtle Lesson/Turtle Fun 1 Template.py
@@ -7,14 +7,26 @@ alex.color("hotpink")
 alex.pensize(5)
 
 
+## Make a box ##
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
 
-alex.forward(50)
-alex.left(90)
-alex.forward(50)
-alex.left(90)
-alex.forward(50)
-alex.left(90)
-alex.forward(50)
-alex.left(90)
 
-wn.mainloop()
+## Make a Triangle ##
+# alex.setheading(60)
+# alex.forward(60)
+# alex.right(120)
+# alex.forward(60)
+# alex.right(120)
+# alex.forward(60)
+
+
+
+
+# wn.mainloop()
--- Turtle Lesson/Turtle Fun 1.py
+++ Turtle Lesson/Turtle Fun 1.py
@@ -2,7 +2,7 @@ from turtle import *
 
 wn = Screen()
 alex = Turtle()
-alex.shape("turtle")
+alex.shape("circle")
 alex.speed(10000)
 a = [[10, 10], [10, 50], [20, 50], [20, 10]]
 
@@ -12,12 +12,11 @@ def drawCoords(listCoords):
     for i in listCoords:
         alex.setposition(i[0], i[1])
         alex.pendown()
-    alex.fillcolor("red")
 
 
 def SquareSpiral(turtleObject):
     turtleObject.hideturtle()
-    for i in range(0, 1000, 10):
+    for i in range(0, 100, 10):
         turtleObject.color("red")
         turtleObject.forward(i + 100)
         turtleObject.left(90)
@@ -27,11 +26,11 @@ def fun(turtleObject):
     turtleObject.color("blue")
     turtleObject.shapesize(0.1)
     turtleObject.penup()
-    size = 0
+    size = 1
     for i in range(3000):
-        turtleObject.stamp()  # Leave an impression on the canvas
-        size += 1  # Increase the size on every iteration
-        turtleObject.forward(size)  # Move tess along
+        turtleObject.stamp()
+        size += 1
+        turtleObject.forward(size)
         turtleObject.right(45+i)
 
 
@@ -46,5 +45,5 @@ def spiral(turtleObject):
         turtleObject.forward(size)
         turtleObject.right(50)
 
-
-SquareSpiral(alex)
+fun(alex)
+wn.mainloop()
