65485003cb99aaeba14f841af34f1ae17c8ebfc9
coolcom200 <leonfattakhov@yahoo.com>
2016-11-27 10:02:01 -0500

    Turtle Graphics in Tkinter window

---
+++ Turtle Lesson/Turtles 2.py
@@ -0,0 +1,42 @@
+from tkinter import *
+import turtle
+
+
+def intelligent_orientation_control():
+    pos = turtle1.heading()
+    if pos == 0:
+        return "w", "a", "d", "s"
+    elif pos == 90:
+        return "w", "a", "d", "s"
+    elif pos == 180:
+        return "w", "a", "d", "s"
+    else:
+        return "w", "d", "a", "s"
+
+
+def keydownHandler(event):
+    a = intelligent_orientation_control()
+    if event.char.lower() == a[0]:
+        turtle1.forward(20)
+    elif event.char.lower() == a[1]:
+        turtle1.right(-90)
+    elif event.char.lower() == a[2]:
+        turtle1.left(-90)
+    elif event.char.lower() == a[3]:
+        turtle1.back(20)
+
+
+root = Tk()
+
+
+frame = Frame(bg='black')
+Label(frame, text='Hello', bg='grey', fg='white').pack(fill='x')
+s = Canvas(frame, width=750, height=750)
+s.pack()
+frame.pack(fill='both', expand=True)
+turtle1 = turtle.RawTurtle(s)
+
+root.bind("<Key>", keydownHandler)
+
+
+root.mainloop()
