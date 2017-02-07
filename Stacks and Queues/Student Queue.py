5283fa12d22e546ca5cff04814c19bce3eafdeec
coolcom200 <leonfattakhov@yahoo.com>
2017-02-06 21:34:00 -0500

    notes

--- Stacks and Queues/Student Queue.py
+++ Stacks and Queues/Student Queue.py
@@ -1,5 +1,6 @@
 class Queue:
     """Stack class represents a first-in-first-out (FIFO) stack of objects"""
+
     def __init__(self):
         self.items = []
 
@@ -10,7 +11,8 @@ class Queue:
         self.items.insert(0, item)
 
     # def dequeue(self):
-    # code this so that it returns and removes the last entry from the Queue
+    # code this so that it returns and removes the first entry from the Queue
+    # in this case it is the last element of the array
 
     def size(self):
         return len(self.items)
